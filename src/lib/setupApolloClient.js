c518c2f8da9fa01e500f22f973aa4d89a5a54164
Horacio Fernandes <horaciofernandes@MacBook-Pro-de-Horacio.local>
2019-02-21 17:23:47 -0300

    Add cache expiration control for each 3 minutes

--- src/lib/setupApolloClient.js
+++ src/lib/setupApolloClient.js
@@ -5,8 +5,10 @@ import { RetryLink } from "apollo-link-retry";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { CachePersistor } from "apollo-cache-persist";
 
+const UPDATE_INTERVAL_MINUTES = 3;
 const SCHEMA_VERSION = "0.0.1";
 const SCHEMA_VERSION_KEY = "apollo-schema-version";
+const LAST_UPDATE = "last-update";
 
 export default async function setupApolloClient() {
   let cache = new InMemoryCache();
@@ -25,9 +27,10 @@ export default async function setupApolloClient() {
 
   return new Promise(resolve => {
     // Read the current schema version from AsyncStorage.
-    const currentVersion = localStorage.getItem(SCHEMA_VERSION_KEY);
+    let currentVersion = localStorage.getItem(SCHEMA_VERSION_KEY);
+    let lastUpdate = localStorage.getItem(LAST_UPDATE);
 
-    if (currentVersion === SCHEMA_VERSION) {
+    if (currentVersion === SCHEMA_VERSION && !isCacheExpired(lastUpdate)) {
       // If the current version matches the latest version,
       // we're good to go and can restore the cache.
       localStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
@@ -36,6 +39,7 @@ export default async function setupApolloClient() {
       // Otherwise, we'll want to purge the outdated persisted cache
       // and mark ourselves as having updated to the latest version.
       localStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+      localStorage.setItem(LAST_UPDATE, new Date().toISOString());
       resolve(persistor.purge());
     }
   }).then(() => {
@@ -44,4 +48,9 @@ export default async function setupApolloClient() {
       link
     });
   });
+}
+
+function isCacheExpired(lastUpdate) {
+  let differenceInMilli = Date.now() - new Date(lastUpdate);
+  return differenceInMilli / 1000 / 60 >= UPDATE_INTERVAL_MINUTES;
 }
